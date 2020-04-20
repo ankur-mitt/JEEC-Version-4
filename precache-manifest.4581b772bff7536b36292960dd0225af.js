940b47fc6dc98f4668dea8ea6b6de00c6b59d39a
ankur-mitt <54750191+ankur-mitt@users.noreply.github.com>
2020-04-20 23:06:37 +0530

    Add files via upload

---
+++ precache-manifest.4581b772bff7536b36292960dd0225af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbc9fa679f49f2806023af5c5f26bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "301c967e4b0fd93ac0a1",
+    "url": "/static/css/2.c4f291ab.chunk.css"
+  },
+  {
+    "revision": "681b545de09b13b1bb59",
+    "url": "/static/css/main.b29694d9.chunk.css"
+  },
+  {
+    "revision": "301c967e4b0fd93ac0a1",
+    "url": "/static/js/2.54f557d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54f557d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681b545de09b13b1bb59",
+    "url": "/static/js/main.8044a59f.chunk.js"
+  },
+  {
+    "revision": "4ae75a2753877608000b",
+    "url": "/static/js/runtime-main.e036ad9c.js"
+  }
+]);
